945836a7261021fabca2ef580adc576f1ad2ad1a
chrismatgit <christianmatabaro@gmail.com>
2020-05-15 22:49:56 +0200

    working on displaying data

--- app.py
+++ app.py
@@ -1,32 +1,10 @@
 from flask import Flask, render_template, request, send_file
-from flask_sqlalchemy import SQLAlchemy
-from send_email import send_email
-from sqlalchemy.sql import func
+from geopy.geocoders import Nominatim
+import pandas
 from werkzeug.utils import secure_filename
 
 # instantiate the object of the class flask
 app = Flask(__name__)
-# configure the connection to the database
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres@localhost/height_collector'
-
-# heroku database creditentials
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://gbidvfwfujdfxa:2b37f7a710abad85e0b15903c3cc14648268931d2db9ddbdc78ed0f1f8869fe3@ec2-54-81-37-115.compute-1.amazonaws.com:5432/da06m423gtjbtf'
-# creating an SQL alchemy object for Flask app
-db = SQLAlchemy(app)
-
-
-class Data(db.Model):  # model from the sqlalchemy
-    """ Class that hundle the database"""
-    __tablename__ = "data"
-    id = db.Column(db.Integer, primary_key=True)
-    email_ = db.Column(db.String(120), unique=True)
-    height_ = db.Column(db.Integer)
-
-    def __init__(self, email_, height_):
-        # constructor
-        self.email_ = email_
-        self.height_ = height_
-
 
 # creating our first route as home page
 @app.route('/')
@@ -35,25 +13,38 @@ def index():
     return render_template('index.html', btn="")
 
 
-@app.route('/success', methods=['POST'])  # To post the data to the server
-def success():
+# To post the data to the server and draw the table
+@app.route('/success-tab', methods=['POST'])
+def success_tab():
     # processing data if request is post
     global file
     if request.method == 'POST':
         file = request.files['file']
-        # saving the file into our directory by adding a certain level of security
-        file.save(secure_filename("uploaded"+file.filename))
-        with open("uploaded"+file.filename, "a") as f:
-            f.write("Just got pissed")
+        try:
+            df = pandas.read_csv(file)
+            gc = Nominatim(scheme='http')
+            df['coordinates'] = df['Address'].apply(gc.geocode)
+            df['Latitude'] = df['coordinates'].apply(
+                lambda x: x.latitude if x != None else None)
+            df['Longitude'] = df['coordinates'].apply(
+                lambda x: x.longitude if x != None else None)
+            df = df.drop("coordinates", 1)
+            # create the filename and formatted it by adding the datetime on it
+            filename = datetime.datetime.now().strftime(
+                "sample_files/%Y-%m-%d-%H-%M-%S-%f"+".csv")
+            df.to_csv(filename, index=None)
+            return render_template("index.html", text=df.to_html(), btn='download.html')
+        except Exception as e:
+            return render_template("index.html", text=str(e), btn="")
 
         # rendering the success page
         return render_template('index.html', btn="download.html")
 
 
-@app.route('/download')  # route to download the csv file
+@app.route('/download-file/')  # route to download the csv file
 def download():
     # function to download the file
-    return send_file("uploaded"+file.filename, attachment_filename="yourfile.csv", as_attachment=True)
+    return send_file(filename, attachment_filename="yourfile.csv", as_attachment=True)
 
 
 # the script is being executed
